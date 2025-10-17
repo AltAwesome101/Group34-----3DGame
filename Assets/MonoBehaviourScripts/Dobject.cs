038032747afb67cf3ad76d5446ad3e5d612134e5
AltAwesome101 <warrenthomasgounden@gmail.com>
2025-10-17 23:10:25 +0200

    BugFixes002

--- Assets/MonoBehaviourScripts/Dobject.cs
+++ Assets/MonoBehaviourScripts/Dobject.cs
@@ -2,8 +2,21 @@ using UnityEngine;
 
 public class Destroyable : MonoBehaviour
 {
+    [Header("Destruction Settings")]
     public int maxHits = 3;
     private int currentHits = 0;
+
+    [Header("Effects")]
+    public ParticleSystem destructionEffect; 
+    public AudioClip breakSound;             
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+    }
 
     public void RegisterHit()
     {
@@ -11,7 +24,23 @@ public class Destroyable : MonoBehaviour
 
         if (currentHits >= maxHits)
         {
-            Destroy(gameObject);
+            BreakObject();
         }
+    }
+
+    void BreakObject()
+    {
+        if (destructionEffect != null)
+        {
+            ParticleSystem effect = Instantiate(destructionEffect, transform.position, Quaternion.identity);
+            effect.Play();
+            Destroy(effect.gameObject, effect.main.duration + effect.main.startLifetime.constantMax);
+        }
+
+       
+        if (breakSound != null)
+            audioSource.PlayOneShot(breakSound);
+
+        Destroy(gameObject);
     }
 }
